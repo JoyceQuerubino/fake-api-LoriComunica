362eeb715eb91e71ba0efed1f63bd1f2fe822736
JoyceQuerubino <joycequerubino5@gmail.com>
2022-03-30 12:11:50 -0300

    Add novas categorias

--- db.js
+++ db.js
@@ -1,34 +1,7 @@
 const itens = require("./data/itens");
 const categories = require("./data/categories");
-const tudo = require("./data/tudo");
 
 module.exports = {
   itens,
   categories,
-  tudo,
 }
-
-
-// const items = require("./data/items");
-
-// module.exports = () => {
-
-//   var tags = items.map((i) => i.tags);
-//   tags = [].concat.apply([], tags);
-//   tags = [...new Set(tags)];
-
-
-
-//   var brands = items.map((i) => i.manufacturer);
-//   brands = [...new Set(brands)];
-
-//   var itemTypes = items.map((i) => i.itemType);
-//   itemTypes = [...new Set(itemTypes)];
-
-//   return {
-//     items,
-//     tags,
-//     brands,
-//     itemTypes,
-//   };
-// };
